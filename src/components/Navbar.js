2ce7bc168db0b66cee2e0ded3797559b1f7d95ed
Kris <kris.fillmore@gmail.com>
2021-06-21 15:09:46 -0700

    intial structure buildout

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Navbar.css"
+
+function navbar() {
+    return (
+        <div className="title">
+            Sudoku
+        </div>
+    )
+}
+
+export default navbar
