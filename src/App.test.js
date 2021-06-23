7aaa8bce1cac650eebe2ffd66dc189495b06b4d8
Kris <kris.fillmore@gmail.com>
2021-06-22 22:37:25 -0700

    basic button tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,18 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Sudoku/);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('Button text displayed', () => {
+  render(<App />);
+
+  const newPuzzleButtonElement = screen.getByText(/New Puzzle/)
+  const solutionButtonElement = screen.getByText(/Solution/)
+  const resetButtonElement = screen.getByText(/Reset/)
+
+  expect(newPuzzleButtonElement).toBeInTheDocument();
+  expect(solutionButtonElement).toBeInTheDocument();
+  expect(resetButtonElement).toBeInTheDocument();
+})
